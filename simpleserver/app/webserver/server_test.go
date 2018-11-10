4e0c13a4845ef87bcbe575d4399d8ce2e650a037
Kari Marttila <kari.marttila@iki.fi>
2018-11-10 21:07:13 +0200

    All /signin test cases implemented but still need to care extra Flag out of JSON response

--- simpleserver/app/webserver/server_test.go
+++ simpleserver/app/webserver/server_test.go
@@ -36,15 +36,10 @@ func TestGetInfo(t *testing.T) {
 	util.LogEnter()
 }
 
-func addTestUser(t *testing.T, bodyMap map[string]interface{}) {
-
-
-}
-
-func TestPostSignin(t *testing.T) {
+func addTestUser(t *testing.T) (recorder *httptest.ResponseRecorder, request *http.Request, testEmail string){
 	util.LogEnter()
 	port := util.MyConfig["port"]
-	testEmail := "jamppa.jamppanen@foo.com"
+	testEmail = "jamppa.jamppanen@foo.com"
     bodyMap := map[string]interface{}{
         "first-name": "Jamppa",
         "last-name": "Jamppanen",
@@ -52,12 +47,20 @@ func TestPostSignin(t *testing.T) {
         "password": "JampanSalasana",
     }
     myBody, _ := json.Marshal(bodyMap)
-    request := httptest.NewRequest("POST", "http://localhost:"+port+"/signin", bytes.NewReader(myBody))
-	recorder := httptest.NewRecorder()
+    request = httptest.NewRequest("POST", "http://localhost:"+port+"/signin", bytes.NewReader(myBody))
+	recorder = httptest.NewRecorder()
 	if status := recorder.Code; status != http.StatusOK {
 		t.Errorf("PostSignin handler returned wrong status code: expected: %v actual: %v",
 			http.StatusOK, status)
 	}
+	util.LogEnter()
+	return recorder, request, testEmail
+}
+
+func TestPostSignin(t *testing.T) {
+	util.LogEnter()
+	// First time adding the user, should go smoothly.
+	recorder, request, testEmail := addTestUser(t)
 	http.HandlerFunc(postSignin).ServeHTTP(recorder, request)
 	responseStr := recorder.Body.String()
 	util.LogDebug("Got response: " + responseStr)
@@ -65,6 +68,27 @@ func TestPostSignin(t *testing.T) {
     err := json.NewDecoder(recorder.Body).Decode(&responseMap)
     if err != nil {
     	t.Errorf("Decoding request failed, err: %s", err.Error())
+	}
+    if responseMap["ret"] != "ok" {
+    	t.Errorf("The response ret values should have been ok, map: %s", responseMap)
+	}
+    if responseMap["email"] != testEmail {
+    	t.Errorf("The response didn't comprise the test email, map: %s", responseMap)
+	}
+	// Second time the user should be in the db already and signin should fail.
+	recorder, request, testEmail = addTestUser(t)
+	http.HandlerFunc(postSignin).ServeHTTP(recorder, request)
+	responseStr = recorder.Body.String()
+	util.LogDebug("Got response: " + responseStr)
+    err = json.NewDecoder(recorder.Body).Decode(&responseMap)
+    if err != nil {
+    	t.Errorf("Decoding request failed, err: %s", err.Error())
+	}
+    if responseMap["ret"] != "failed" {
+    	t.Errorf("The response ret values should have been failed, map: %s", responseMap)
+	}
+    if responseMap["msg"] != "Email already exists: " + testEmail {
+    	t.Errorf("The response error msg was not correct, map: %s", responseMap)
 	}
     if responseMap["email"] != testEmail {
     	t.Errorf("The response didn't comprise the test email, map: %s", responseMap)
